d292320c6acf51506c44cdf176ac80c6e6244f1b
Abner <abnervg37@gmail.com>
2021-05-31 21:05:28 -0300

    Project added
    Adicionado projeto do curso "Desenvolvimento de aplicações com .NET".

---
+++ src/Desenvolvimento-de-aplicacoes-com-dotNET/AspNetMVC/AspNetMVC/Models/Context.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetMVC.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> produtos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;database=AspNetMVC;Integrated Security=True;");
+
+            //base.OnConfiguring(optionsBuilder);
+        }
+
+        public virtual void SetModified(object entity)
+        {
+            Entry(entity).State = EntityState.Modified;
+        }
+    }
+}
---
+++ src/Desenvolvimento-de-aplicacoes-com-dotNET/AspNetMVC/AspNetMVC/Models/Produto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetMVC.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage = "Este campo é obrigatório.")]
+        public string Descricao { get; set; }
+        
+        [Required(ErrorMessage = "Este campo é obrigatório.")]
+        public int Quantidade { get; set; }
+
+        [Display(Name = "Categoria")]
+        public int CategoriaId { get; set; }
+        public Categoria Categoria { get; set; }
+    }
+}
